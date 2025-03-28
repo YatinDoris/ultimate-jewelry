ec15f7e5324d0f3eaadc764280809efcaec07af4
utsav <utsavdoris@gmail.com>
2025-03-28 14:40:55 +0530

    update

--- userpanel/src/app/components/dynamiComponents.jsx
+++ userpanel/src/app/components/dynamiComponents.jsx
@@ -4,9 +4,13 @@ import dynamic from "next/dynamic";
 // Common Component
 export const Header = dynamic(() => import("./header"), { ssr: false });
 export const Footer = dynamic(() => import("./footer"), { ssr: false });
-// export const CustomImage = dynamic(() => import("./customImage"), {
-//   ssr: false,
-// });
+export const ProgressiveImg = dynamic(() => import("./progressive-img"), {
+  ssr: false,
+});
+
+export const CustomImg = dynamic(() => import("./custom-img"), {
+  ssr: false,
+});
 
 export const Layout = dynamic(() => import("./layout"), { ssr: false });
 export const Lenis = dynamic(() => import("./lenis"), { ssr: false });
