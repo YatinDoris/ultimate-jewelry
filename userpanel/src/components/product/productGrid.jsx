d145a46f5b659164229ca62a6d8a32a094b961e4
utsav <utsavdoris@gmail.com>
2025-04-04 15:31:01 +0530

    product filter

--- userpanel/src/components/product/productGrid.jsx
+++ userpanel/src/components/product/productGrid.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { memo, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import useQueryParams from "@/hooks/useQueryParams";
 import ProductCard from "./productCard";
@@ -9,10 +9,19 @@ import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentPage } from "@/store/slices/productSlice";
 import SkeletonLoader from "../skeletonLoader";
-import FilterMenu from "./filterMenu";
-
+import { VscSettings } from "react-icons/vsc";
+import { AiOutlineClose } from "react-icons/ai";
+import { FiMinus, FiPlus } from "react-icons/fi";
 const ITEMS_PER_PAGE = 20;
 
+const sortByList = [
+  { value: "date_new_to_old", title: "NEW TO OLD" },
+  { value: "date_old_to_new", title: "OLD TO NEW" },
+  { value: "price_high_to_low", title: "HIGH TO LOW" },
+  { value: "price_low_to_high", title: "LOW TO HIGH" },
+  { value: "alphabetically_a_to_z", title: "A-Z" },
+  { value: "alphabetically_z_to_a", title: "Z-A" },
+];
 const ProductGrid = memo(
   ({
     productList = [],
@@ -24,11 +33,14 @@ const ProductGrid = memo(
   }) => {
     const queryParams = useQueryParams();
     const dispatch = useDispatch();
+
     const { columnCount } = useWindowSize();
     const { currentPage } = useSelector(({ product }) => product);
     const [selectedSortByValue, setSelectedSortByValue] =
       useState("date_new_to_old");
     const [selectedVariation, setSelectedVariation] = useState({});
+    const [showSidebar, setShowSidebar] = useState(false);
+    const [activeIndex, setActiveIndex] = useState("sortBy");
 
     const handlePageClick = ({ selected }) => {
       dispatch(setCurrentPage(selected));
@@ -80,6 +92,7 @@ const ProductGrid = memo(
     const handleClearFilters = () => {
       setSelectedSortByValue("date_new_to_old");
       setSelectedVariation({});
+      setActiveIndex("sortBy");
     };
     const sortBySelectHandler = (eventKey) => {
       setSelectedSortByValue(eventKey);
@@ -92,6 +105,9 @@ const ProductGrid = memo(
       setSelectedVariation(updatedFilters);
     };
 
+    const toggleFAQ = (index) => {
+      setActiveIndex(index === activeIndex ? null : index);
+    };
     let filteredItemsList = productList;
     if (Object.keys(selectedVariation)?.length) {
       filteredItemsList = productList.filter((product) => {
@@ -104,20 +120,26 @@ const ProductGrid = memo(
         });
       });
     }
-    [...filteredItemsList].sort((a, b) => {
+    filteredItemsList = [...filteredItemsList].sort((a, b) => {
       if (selectedSortByValue === "alphabetically_a_to_z") {
         return a.productName.localeCompare(b.productName);
-      } else if (selectedSortByValue === "alphabetically_z_to_a") {
-        return b.productName.localeCompare(a.productName);
-      } else if (selectedSortByValue === "price_low_to_high") {
-        return parseFloat(a.baseSellingPrice) - parseFloat(b.baseSellingPrice);
-      } else if (selectedSortByValue === "price_high_to_low") {
-        return parseFloat(b.baseSellingPrice) - parseFloat(a.baseSellingPrice);
-      } else if (selectedSortByValue === "date_old_to_new") {
-        return a.createdDate - b.createdDate;
-      } else if (selectedSortByValue === "date_new_to_old") {
-        return b.createdDate - a.createdDate;
       }
+      if (selectedSortByValue === "alphabetically_z_to_a") {
+        return b.productName.localeCompare(a.productName);
+      }
+      if (selectedSortByValue === "price_low_to_high") {
+        return parseFloat(a.baseSellingPrice) - parseFloat(b.baseSellingPrice);
+      }
+      if (selectedSortByValue === "price_high_to_low") {
+        return parseFloat(b.baseSellingPrice) - parseFloat(a.baseSellingPrice);
+      }
+      if (selectedSortByValue === "date_old_to_new") {
+        return new Date(a.createdDate) - new Date(b.createdDate);
+      }
+      if (selectedSortByValue === "date_new_to_old") {
+        return new Date(b.createdDate) - new Date(a.createdDate);
+      }
+      return 0;
     });
 
     const pageCount = Math.ceil(filteredItemsList.length / ITEMS_PER_PAGE);
@@ -136,6 +158,9 @@ const ProductGrid = memo(
       return `${basePath}${queryString}`;
     };
 
+    useEffect(() => {
+      setSelectedSortByValue(selectedSortByValue);
+    }, [selectedSortByValue]);
     return (
       <>
         {isLoading ? (
@@ -153,32 +178,186 @@ const ProductGrid = memo(
           </div>
         ) : (
           <div>
-            <FilterMenu
-              filterMenuList={uniqueVariations}
-              selectedSortByValue={selectedSortByValue}
-              sortBySelectHandler={sortBySelectHandler}
-              onSelectVariant={onSelectVariant}
-              selectedVariation={selectedVariation}
-              handleClearFilters={handleClearFilters}
-            />
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 6xl:grid-cols-6 gap-x-4 gap-y-6 place-items-center">
-              {currentProducts.map((product) => (
-                <ProductCard
-                  key={`product-key-${product?.productName}`}
-                  title={product?.productName}
-                  discount={product?.discount}
-                  basePrice={product?.basePrice}
-                  img={product?.images?.[0]?.image}
-                  video={product?.video}
-                  price={product?.baseSellingPrice}
-                  goldColorVariations={product?.goldColorVariations}
-                  productLink={getProductLink({
-                    queryParams,
-                    isDiamondSettingPage,
-                    product,
-                  })}
-                />
-              ))}
+            {/* Filter */}
+            <div
+              className={`flex  ${
+                showSidebar ? "justify-end" : "justify-between"
+              }  mb-6`}
+            >
+              <button
+                onClick={() => setShowSidebar(true)}
+                className={`${
+                  showSidebar ? "hidden" : "block"
+                } flex items-center gap-2 px-4 py-2 border shadow-sm bg-primary text-white hover:bg-gray-100 hover:border-primary hover:text-primary font-medium transition-all duration-300`}
+              >
+                <VscSettings className="text-xl" /> Filter
+              </button>
+              {filteredItemsList ? (
+                <span> {filteredItemsList.length} items</span>
+              ) : null}
+            </div>
+            <div className="flex gap-6 items-start">
+              {/* filter */}
+              <div
+                className={`w-72  bg-transparent ${
+                  showSidebar
+                    ? "translate-x-0 block"
+                    : "-translate-x-full hidden"
+                }`}
+              >
+                {/* Header */}
+                <div className="flex justify-between items-center border-b border-[#C8C8C6] pb-4">
+                  <h2 className="text-lg font-semibold">All Filter</h2>
+                  <button
+                    onClick={() => {
+                      setShowSidebar(false);
+                      setActiveIndex("sortBy");
+                    }}
+                  >
+                    <AiOutlineClose />
+                  </button>
+                </div>
+
+                {/* Clear Filters Button */}
+                <div className="flex justify-end">
+                  <button
+                    onClick={handleClearFilters}
+                    className="text-xs underline hover:underline mt-3 "
+                  >
+                    Reset Filters
+                  </button>
+                </div>
+
+                {/* Filters Content */}
+                <div>
+                  <div className="border-b border-[#C8C8C6] ">
+                    <button
+                      className={`w-full flex items-center justify-between ${
+                        activeIndex === "sortBy" ? "pt-4 pb-2" : "py-4"
+                      }`}
+                      onClick={() => toggleFAQ("sortBy")}
+                    >
+                      <p className="font-semibold mb-1">Sort By</p>
+                      <span className="text-xl">
+                        {activeIndex === "sortBy" ? <FiMinus /> : <FiPlus />}
+                      </span>
+                    </button>
+                    <div
+                      className={`transition-all duration-300 ease-in-out overflow-hidden ${
+                        activeIndex === "sortBy"
+                          ? "max-h-screen opacity-100"
+                          : "max-h-0 opacity-0"
+                      }`}
+                    >
+                      <div className="flex flex-wrap gap-2 pb-4">
+                        {sortByList.map((item) => (
+                          <button
+                            key={item.value}
+                            onClick={() => sortBySelectHandler(item.value)}
+                            className={`px-3 py-1 border rounded-md text-sm ${
+                              selectedSortByValue === item.value
+                                ? "bg-primary text-white"
+                                : "bg-gray-100 hover:bg-gray-200"
+                            }`}
+                          >
+                            {item.title}
+                          </button>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Variations Filter */}
+                  {uniqueVariations?.map((x, index) => (
+                    <div
+                      key={x?.variationId}
+                      className="border-b border-[#C8C8C6] "
+                    >
+                      <button
+                        className={`w-full flex items-center justify-between  ${
+                          activeIndex === index ? "pt-4 pb-2" : "py-4"
+                        }`}
+                        onClick={() => toggleFAQ(index)}
+                      >
+                        <p className="font-semibold mb-1">{x?.variationName}</p>
+                        <span className="text-xl">
+                          {activeIndex === index ? <FiMinus /> : <FiPlus />}
+                        </span>
+                      </button>
+                      <div
+                        className={`transition-all duration-300 ease-in-out overflow-hidden ${
+                          activeIndex === index
+                            ? "max-h-screen opacity-100"
+                            : "max-h-0 opacity-0"
+                        }`}
+                      >
+                        <div className="flex flex-wrap gap-2 pb-4">
+                          {x?.variationTypes?.map((y) => {
+                            return (
+                              <button
+                                key={`${x.variationId}-${y.variationTypeName}`}
+                                onClick={() =>
+                                  onSelectVariant(
+                                    x?.variationName,
+                                    y?.variationTypeName
+                                  )
+                                }
+                                className={`px-3 py-1 m-1 border rounded-md text-sm flex items-center justify-center ${
+                                  selectedVariation[x?.variationName] ===
+                                  y?.variationTypeName
+                                    ? "bg-primary text-white"
+                                    : "bg-gray-100 hover:bg-gray-200"
+                                }  ${
+                                  selectedVariation[x?.variationName] ===
+                                    y?.variationTypeName &&
+                                  y.variationTypeHexCode
+                                    ? "border-2 border-primary"
+                                    : ""
+                                }`}
+                                style={
+                                  y?.variationTypeHexCode
+                                    ? {
+                                        backgroundColor: y.variationTypeHexCode,
+                                        width: "32px",
+                                        height: "32px",
+                                      }
+                                    : {}
+                                }
+                              >
+                                {!y?.variationTypeHexCode &&
+                                  y?.variationTypeName}
+                              </button>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div
+                className={`grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 ${
+                  showSidebar ? "lg:grid-cols-3" : "lg:grid-cols-4"
+                } 6xl:grid-cols-6 gap-x-4 gap-y-6 place-items-center`}
+              >
+                {currentProducts.map((product) => (
+                  <ProductCard
+                    key={`product-key-${product?.productName}`}
+                    title={product?.productName}
+                    discount={product?.discount}
+                    basePrice={product?.basePrice}
+                    img={product?.images?.[0]?.image}
+                    video={product?.video}
+                    price={product?.baseSellingPrice}
+                    goldColorVariations={product?.goldColorVariations}
+                    productLink={getProductLink({
+                      queryParams,
+                      isDiamondSettingPage,
+                      product,
+                    })}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         )}
