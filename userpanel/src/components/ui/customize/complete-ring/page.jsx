8c66abab33b03d3d46b8980805787ef237cef583
parthd01 <parthdoris01@gmail.com>
2025-04-25 12:46:11 +0530

    3 steps grid

--- userpanel/src/components/ui/customize/complete-ring/page.jsx
+++ userpanel/src/components/ui/customize/complete-ring/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 import { fetchCustomizeProducts } from "@/_actions/customize.action";
-import { ProductDetailPage, ProductGrid } from "@/components/dynamiComponents";
+import {
+  ProductDetailPage,
+  ProductNotFound,
+} from "@/components/dynamiComponents";
 import { useCallback, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import StepsGrid from "../StepsGrid";
@@ -9,15 +12,19 @@ import diamond from "@/assets/images/customize/customize-diamond-black.svg";
 import ringWithDiamondBlack from "@/assets/images/customize/customize-ringWithDiamond-black.svg";
 import { setCustomProductDetails } from "@/store/slices/commonSlice";
 import { helperFunctions } from "@/_helper";
+import { useRouter } from "next/navigation";
 export default function SelectDiamondPage() {
   const { customProductDetails } = useSelector(({ common }) => common);
+  const router = useRouter();
   const dispatch = useDispatch();
   useEffect(() => {
     const customProduct = helperFunctions.getCustomProduct();
     if (customProduct) {
       dispatch(setCustomProductDetails(customProduct));
+    } else {
+      router.replace("/customize/start-with-setting");
     }
-  }, [dispatch]);
+  }, [dispatch, router]);
   const pId = customProductDetails?.productId;
   const isDiamondSelected = customProductDetails?.diamondDetails;
   const currentStep = 3;
@@ -60,12 +67,39 @@ export default function SelectDiamondPage() {
           disabled: false,
         },
       ];
-    } else {
+    } else if (pId && !isDiamondSelected) {
+      return [
+        {
+          id: 1,
+          label: "Choose Setting",
+          icon: ring,
+          subOption: [
+            {
+              label: "View",
+              route: `/customize/start-with-setting/${pId}`,
+            },
+            {
+              label: "Change",
+              route: "/customize/start-with-setting",
+            },
+          ],
+        },
+        {
+          id: 2,
+          label: "Choose Diamond",
+          icon: diamond,
+        },
+
+        {
+          id: 3,
+          label: "Complete Ring",
+          icon: ringWithDiamondBlack,
+          iconBlack: ringWithDiamondBlack,
+          disabled: true,
+        },
+      ];
     }
   }, [pId, isDiamondSelected]);
-  const { customizeProductList, customizeProductLoading } = useSelector(
-    ({ product }) => product
-  );
 
   const loadData = useCallback(async () => {
     await dispatch(fetchCustomizeProducts());
@@ -77,12 +111,18 @@ export default function SelectDiamondPage() {
 
   return (
     <>
-      <section className="container pt-8">
-        <StepsGrid steps={steps} currentStep={currentStep} />
-      </section>
-      <section className="pt-10 md:pt-14 lg:pt-10 2xl:pt-12">
-        <ProductDetailPage customizePage="completeRing" />
-      </section>
+      {customProductDetails && Object.keys(customProductDetails).length > 0 ? (
+        <>
+          <section className="container pt-8">
+            <StepsGrid steps={steps} currentStep={currentStep} />
+          </section>
+          <section className="pt-10 md:pt-14 lg:pt-10 2xl:pt-12">
+            <ProductDetailPage customizePage="completeRing" />
+          </section>
+        </>
+      ) : (
+        <ProductNotFound textClassName="px-4 md:px-8 w-full md:w-[50%] lg:w-[35%] 2xl:w-[32%]" />
+      )}
     </>
   );
 }
