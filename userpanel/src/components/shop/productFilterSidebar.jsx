d3a52821ca09892efcd10e1346fd377412321a83
utsav <utsavdoris@gmail.com>
2025-05-01 09:35:14 +0530

    header and banner make dynamic

--- userpanel/src/components/shop/productFilterSidebar.jsx
+++ userpanel/src/components/shop/productFilterSidebar.jsx
@@ -433,16 +433,23 @@ export default function ProductFilterSidebar({ uniqueVariations = [] }) {
               }`}
             >
               <div className="space-y-4 mt-5">
-                <RangeSlider
-                  defaultValue={uniqueFilterOptions.availablePriceRange}
-                  min={uniqueFilterOptions.availablePriceRange[0]}
-                  max={uniqueFilterOptions.availablePriceRange[1]}
-                  rangeValue={values.priceRange}
-                  setRangeValue={(value) => setFieldValue("priceRange", value)}
-                  setInputValues={(value) => setFieldValue("priceRange", value)}
-                  step={1}
-                  renderTrack={multipleTrack}
-                />
+                {uniqueFilterOptions?.length ? (
+                  <RangeSlider
+                    defaultValue={uniqueFilterOptions?.availablePriceRange}
+                    min={uniqueFilterOptions?.availablePriceRange[0]}
+                    max={uniqueFilterOptions?.availablePriceRange[1]}
+                    rangeValue={values.priceRange}
+                    setRangeValue={(value) =>
+                      setFieldValue("priceRange", value)
+                    }
+                    setInputValues={(value) =>
+                      setFieldValue("priceRange", value)
+                    }
+                    step={1}
+                    renderTrack={multipleTrack}
+                  />
+                ) : null}
+
                 <div className="flex justify-between gap-4">
                   <input
                     type="text"
